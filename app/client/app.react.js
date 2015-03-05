edb186e9ffade55699f7bd9521d32c9751120a83
Davis Hunt <vdh3@mac.com>
2015-03-04 18:55:00 -0600

    Priming the pump.

--- app/client/app.react.js
+++ app/client/app.react.js
@@ -1,9 +1,26 @@
 /** @jsx React.DOM */
 
-var React = require('react'),
+var $ = require('jquery'),
+	React = require('react'),
+	moment = require('moment'),
 	ShowGrid = require('./components/ShowGrid.react');
 
-React.render(
-	<ShowGrid day={ initialState.day } range={ initialState.range } venues={ initialState.venues }></ShowGrid>,
-	document.getElementById('showgrid')
-);
+
+var dataURL = 'http://localhost:8000/i/grid/';
+$.ajax({
+	type: "GET",
+	url: dataURL,
+}).success(function(data, status) {
+
+	var venues = data,
+		day = moment().format('MMMM Do YYYY').toString(),
+		range = 7;
+
+	React.render(
+		<ShowGrid venues={ venues } day={ day } range={ range }></ShowGrid>,
+		document.getElementById('showgrid')
+	);
+
+});
+
+
