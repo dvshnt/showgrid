6cb7ffad78574ad6e3e0d9d3d19a8b5bfd3af70a
Davis Hunt <vdh3@mac.com>
2015-03-04 23:10:43 -0600

    Footer added and base font color changed to #333.

--- app/client/components/Footer.react.js
+++ app/client/components/Footer.react.js
@@ -4,12 +4,33 @@ var React = require('react');
 
 module.exports = Header = React.createClass({
 	render: function() {
+		var cookieStyle = {
+			position: 'absolute',
+			left: '-5000px'
+		};
+
 		return (
-			<header>
-				<a href="/">
-					<span className="head--title">SG</span>
-				</a>
-			</header>
+			<footer>
+				<div className="input--prompt">
+					<h3>something missing?</h3>
+					<p>Let us know if you notice a show or venue missing from the Grid. Otherwise, feel free to drop us a line and let us know what you think of the thing. We are in the early stages and welcome any and all feedback.</p>
+					<p>Email us at <a href="mailto:showgridnashville@gmail.com">showgridnashville@gmail.com</a> with any requests or comments.</p>
+				</div>
+				<div className='email--form'>
+					<div id="mc_embed_signup">
+						<form action="//showgrid.us9.list-manage.com/subscribe/post?u=24f724359c2dd0e5e6d775b61&amp;id=49b59ce078" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<div id="mc_embed_signup_scroll">
+								<label for="mce-EMAIL">Receive news and updates about Show Grid</label><br></br>
+								<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="Email" required/>
+								<div style={ cookieStyle }>
+									<input type="text" name="b_24f724359c2dd0e5e6d775b61_49b59ce078" tabindex="-1" value=""/>
+								</div>
+								<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"/>
+							</div>
+						</form>
+					</div>
+				</div>
+			</footer>
 		)
 	}
 });
