ce9dedcb9b79c9a9e9560bd5df8fffeab10ca25f
Davis Hunt <vdh3@mac.com>
2015-03-09 14:04:02 -0500

    Temporary Mailchimp fix.

--- app/client/components/Footer.react.js
+++ app/client/components/Footer.react.js
@@ -21,11 +21,11 @@ module.exports = Header = React.createClass({
 						<form action="//showgrid.us9.list-manage.com/subscribe/post?u=24f724359c2dd0e5e6d775b61&amp;id=49b59ce078" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 							<div id="mc_embed_signup_scroll">
 								<label for="mce-EMAIL">Receive news and updates about Show Grid</label><br></br>
-								<input type="text" value="" name="EMAIL" id="mce-EMAIL" placeholder="Email" required/>
+								<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="Email"/>
 								<div style={ cookieStyle }>
 									<input type="text" name="b_24f724359c2dd0e5e6d775b61_49b59ce078" tabindex="-1" value=""/>
 								</div>
-								<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"/>
+								<input type="submit" value="Join the Mailing List" name="subscribe" id="mc-embedded-subscribe"/>
 							</div>
 						</form>
 					</div>
