e1c6ca10bf43b3721014fc81027dbca3286b0dce
Davis Hunt <vdh3@mac.com>
2015-03-13 13:35:52 -0500

    New admin interface.

--- app/server/admin.py
+++ app/server/admin.py
@@ -1,6 +1,10 @@
 from models import *
 from django.contrib import admin
 
+class ShowAdmin(admin.ModelAdmin):
+	search_fields = ['band_name']
+	list_display = ('date', 'band_name', 'venue')
+
 admin.site.register(Venue)
-admin.site.register(Show)
+admin.site.register(Show, ShowAdmin)
 admin.site.register(Address)
