179aeea5dbba3a1bd5a6b72ff9bee9172078fed3
Davis Hunt <vdh3@mac.com>
2015-03-24 22:25:39 -0500

    Define search API URL.

--- app/server/urls.py
+++ app/server/urls.py
@@ -1,19 +1,21 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     url(r'^$', 'server.views.index', name='index'),
     url(r'^check/venues$', 'server.views.check_venues', name='check_venues'),
+
+    url(r'^i/search$', 'server.views.get_search_results', name='search results'),
     
  	url(r'^i/grid/$', 'server.views.grid', name='grid'),
 	url(r'^i/grid/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)$', 'server.views.grid', name='grid')
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# ... the rest of your URLconf goes here ...
+
 urlpatterns += staticfiles_urlpatterns()
