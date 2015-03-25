a337abb38fb29a49546cac6cfebd7effa033194e
Davis Hunt <vdh3@mac.com>
2015-03-24 22:28:39 -0500

    Redefined Calendar component.

--- app/client/components/Calendar.react.js
+++ app/client/components/Calendar.react.js
@@ -1,19 +1,52 @@
 /** @jsx React.DOM */
-var React = require('react/addons'),
-	CalendarRow = React.createFactory(require('./CalendarRow.react'));
+var $ = require('jquery'),
+	React = require('react'),
+
+	Swipeable = React.createFactory(require('react-swipeable')),
+	moment = require('moment'),
+
+	Header = React.createFactory(require('./Header.react')),
+	TableHead = React.createFactory(require('./TableHead.react')),
+	TableBody = React.createFactory(require('./TableBody.react')),
+	Footer = React.createFactory(require('./Footer.react')),
+
+	GridEngine = require('../util/GridEngine'),
+	DateManager = require('../util/DateManager');
+
+
+React.initializeTouchEvents(true);
 
 module.exports = Calendar = React.createClass({
-	render: function() {
-		var days = this.props.days;
 
+	render: function() {
 		return (
-			<section id="calendar">
-				{
-					this.props.venues.map(function(venue) {
-						return <CalendarRow key={ venue.id } days={ days } venue={ venue }/>
-    				})
-				}
-			</section>
+			<div className="calendar--container">
+				<div className="arrow previous" onClick={ this.props.previousPage }>
+					<div className="direction"><div className="top"></div><div className="bottom"></div></div>
+				</div>
+				<div className="arrow next" onClick={ this.props.nextPage }>
+					<div className="direction"><div className="top"></div><div className="bottom"></div></div>
+				</div>
+
+				<section id="grid--fixed--container">
+
+					<Header></Header>
+					<TableHead days={ this.props.days }/>
+
+				</section>
+				
+				<Swipeable  
+					onSwipedLeft={ this.nextPage } 
+					onSwipedRight={ this.previousPage } >
+				
+					<TableBody
+						days={ this.props.days }
+						venues={ this.props.venues }/>
+
+				</Swipeable>
+				
+				<Footer></Footer>
+			</div>
 		)
 	}
 });
