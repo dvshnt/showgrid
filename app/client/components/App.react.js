19d9c111e2dcdb22fd7a5156c1acd3d8cbf0b731
Davis Hunt <vdh3@mac.com>
2015-04-06 15:28:41 -0500

    Update structure for React-router.

---
+++ app/client/components/App.react.js
@@ -0,0 +1,140 @@
+/** @jsx React.DOM */
+var $ = require('jquery'),
+	React = require('react'),
+	Router = require('react-router'),
+	moment = require('moment'),
+
+	GridEngine = require('../util/GridEngine'),
+	DateManager = require('../util/DateManager');
+
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+module.exports = App = React.createClass({
+	mixins: [Router.State, Router.Navigation],
+
+	getInitialState: function() {
+		var venues = GridEngine.getVenues(),
+			range = GridEngine.getCellCount(),
+			days = DateManager.getDaysArray(moment(), range);
+
+		return {
+			venues: venues,
+			range: range,
+			days: days
+		};
+	},
+
+	componentDidMount: function() {
+		var _this = this;
+
+		window.addEventListener("resize", function(e) {
+			GridEngine.calculateCellCount();
+
+			_this.setState({ 
+				range: GridEngine.getCellCount()
+			});
+		});
+	},
+
+	componentWillUpdate: function(nextProps, nextState) {
+		// If range changes, we need to update the days array
+		if (nextState.range !== this.state.range) {
+			var start = moment(this.state.days[0].date, 'MMMM Do YYYY');
+
+			var days = DateManager.getDaysArray(start, nextState.range);
+
+			this.setState({
+				range: nextState.range,
+				days: days
+			});
+		}
+
+	},
+
+	nextPage: function () {
+		var _this = this;
+
+		// Get current calendar range
+		var offset = this.state.range;
+
+		// Get the last day that we will use to calculate the next page's days
+		var day = DateManager.getStartOfNextPage(this.state.days[offset - 1].date);
+
+		var dataURL = GridEngine.domain + '/i/grid/' + 
+						day.format('YYYY') + '/' + 
+						day.format('M') + '/' + 
+						day.format('D') + '?range=' + offset;
+		
+
+		$.ajax({
+			type: "GET",
+			url: dataURL,
+		}).success(function(data, status) {
+			var days = DateManager.getDaysArray(day, offset);
+
+			_this.setState({ 
+				venues: data,
+				days: days
+			});
+		});
+	},
+
+	previousPage: function () {
+		var _this = this;
+
+		// Get current calendar range
+		var offset = this.state.range;
+
+		// Get the last day that we will use to calculate the next page's days
+		var day = DateManager.getStartOfPreviousPage(this.state.days[0].date, offset);
+
+		var dataURL = GridEngine.domain + '/i/grid/' + 
+						day.format('YYYY') + '/' + 
+						day.format('M') + '/' + 
+						day.format('D') + '?range=' + offset;
+		
+
+		$.ajax({
+			type: "GET",
+			url: dataURL,
+		}).success(function(data, status) {
+			var days = DateManager.getDaysArray(day, offset);
+
+			_this.setState({ 
+				venues: data,
+				days: days
+			});
+		});
+	},
+
+	changeMode: function() {
+		var nav = $(".nav--button");
+
+		if (nav.hasClass("search")) {
+			this.transitionTo("/");
+			return;
+		}
+
+		this.transitionTo("/search");
+		return;
+	},
+
+	render: function() {
+		var classes = "nav--button ";
+			classes += (this.getPath() === "/") ? "calendar" : "search";
+
+		return (
+			<div>
+				<div className={ classes } onClick={ this.changeMode }></div>
+				<RouteHandler 
+					venues={ this.state.venues } 
+					range={ this.state.range } 
+					days={ this.state.days }
+					next={ this.nextPage }
+					previous={ this.previousPage }/>
+			</div>
+		)
+	}
+});
--- app/client/components/ShowGrid.react.js
+++ app/client/components/ShowGrid.react.js
@@ -2,8 +2,6 @@
 var $ = require('jquery'),
 	React = require('react'),
 
-	moment = require('moment'),
-
 	Swipeable = React.createFactory(require('react-swipeable')),
 
 	ControlNext = React.createFactory(require('./ControlNext.react')),
@@ -11,111 +9,20 @@ var $ = require('jquery'),
 
 	Header = React.createFactory(require('./Header.react')),
 	Calendar = React.createFactory(require('./Calendar.react')),
-	Footer = React.createFactory(require('./Footer.react')),
-
-	GridEngine = require('../util/GridEngine'),
-	DateManager = require('../util/DateManager');
+	Footer = React.createFactory(require('./Footer.react'));
 
 
 React.initializeTouchEvents(true);
 
 module.exports = ShowGrid = React.createClass({
-
-	componentDidMount: function() {
-		var _this = this;
-
-		GridEngine.init();
-
-		this.setProps({ range: GridEngine.getCellCount() });
-
-		window.addEventListener("resize", function(e) {
-			GridEngine.calculateCellCount();
-
-			_this.setProps({ 
-				range: GridEngine.getCellCount()
-			});
-		});
-	},
-
-	componentWillReceiveProps: function(nextProps) {
-		// If range changes, we need to update the days array
-		if (nextProps.range !== this.props.range) {
-			var start = moment(this.props.days[0].date, 'MMMM Do YYYY');
-
-			var days = DateManager.getDaysArray(start, nextProps.range);
-
-			this.setProps({
-				range: nextProps.range,
-				days: days
-			});
-		}
-
-	},
-
-	nextPage: function () {
-		var _this = this;
-
-		// Get current calendar range
-		var offset = this.props.range;
-
-		// Get the last day that we will use to calculate the next page's days
-		var day = DateManager.getStartOfNextPage(this.props.days[offset - 1].date);
-
-		var dataURL = GridEngine.domain + '/i/grid/' + 
-						day.format('YYYY') + '/' + 
-						day.format('M') + '/' + 
-						day.format('D') + '?range=' + offset;
-		
-
-		$.ajax({
-			type: "GET",
-			url: dataURL,
-		}).success(function(data, status) {
-			var days = DateManager.getDaysArray(day, offset);
-
-			_this.setProps({ 
-				venues: data,
-				days: days
-			});
-		});
-	},
-
-	previousPage: function () {
-		var _this = this;
-
-		// Get current calendar range
-		var offset = this.props.range;
-
-		// Get the last day that we will use to calculate the next page's days
-		var day = DateManager.getStartOfPreviousPage(this.props.days[0].date, offset);
-
-		var dataURL = GridEngine.domain + '/i/grid/' + 
-						day.format('YYYY') + '/' + 
-						day.format('M') + '/' + 
-						day.format('D') + '?range=' + offset;
-		
-
-		$.ajax({
-			type: "GET",
-			url: dataURL,
-		}).success(function(data, status) {
-			var days = DateManager.getDaysArray(day, offset);
-
-			_this.setProps({ 
-				venues: data,
-				days: days
-			});
-		});
-	},
-
 	render: function() {
 		return (
 			<section id="container">
-				<ControlNext next={ this.nextPage }/>
-				<ControlPrevious previous={ this.previousPage }/>
+				<ControlNext next={ this.props.next }/>
+				<ControlPrevious previous={ this.props.previous }/>
 
 				<Header days={ this.props.days } />
-				<Swipeable onSwipedLeft={ this.nextPage } onSwipedRight={ this.previousPage }>
+				<Swipeable onSwipedLeft={ this.props.next } onSwipedRight={ this.props.previous }>
 					<Calendar venues={ this.props.venues } days={ this.props.days }/>
 					<Footer/>
 				</Swipeable>
