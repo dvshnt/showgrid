85628e1d07baa550aa57c2563bf4de1686174286
Davis Hunt <vdh3@mac.com>
2015-04-06 15:26:24 -0500

    Fix header.

--- app/client/components/HeaderPage.react.js
+++ app/client/components/HeaderPage.react.js
@@ -5,9 +5,7 @@ module.exports = HeaderPage = React.createClass({
 	render: function() {
 		return (
 			<section id="header--page">
-				<a href="/">
-					<h1 className="header--page__title" alt="Showgrid">SG</h1>
-				</a>
+				<a href="/">SG</a>
 			</section>
 		)
 	}
