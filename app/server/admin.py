98b79d497edb8c168fbc527ccafa32ce87c7e68d
Davis Hunt <vdh3@mac.com>
2015-04-14 07:51:12 -0500

    Step one: model refactor.

--- app/server/admin.py
+++ app/server/admin.py
@@ -5,6 +5,15 @@ class ShowAdmin(admin.ModelAdmin):
 	search_fields = ['band_name']
 	list_display = ('date', 'band_name', 'venue')
 
+# class ShowAdmin(admin.ModelAdmin):
+# 	search_fields = ['headliners', 'openers', 'title', 'venue']
+# 	list_display = ('date', 'headliners', 'openers', 'venue')
+
 admin.site.register(Venue)
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Address)
+
+# admin.site.register(Venue_v2)
+# admin.site.register(Show_v2, ShowAdmin)
+# admin.site.register(Address_v2)
+
--- app/server/models.py
+++ app/server/models.py
@@ -1,8 +1,119 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from django.contrib.localflavor.us.models import USStateField
 
+
+class Venue_v2(models.Model):
+	name = models.CharField(max_length=200)
+	address = models.ForeignKey('Address_v2')
+	image = models.ImageField (upload_to='showgrid/img/venues/')
+	website = models.URLField()
+
+	# Sign-post for if venue is open or not
+	opened = models.BooleanField(default=True)
+
+	# Auto-fill URL for calendars without unique links
+	autofill = models.CharField(max_length=200, blank=True)
+
+	def __unicode__ (self):
+		return self.name
+
+	def json(self):
+		return {
+			'id' : self.id,
+			'name' : self.name,
+			'website' : self.website,
+			'image' : self.image.url,
+			'address' : self.address.json()
+		}
+
+	@property
+	def alphabetical_title(self):
+		name = self.name
+		starts_with_flags = ['the ', 'an ', 'a ']
+
+		for flag in starts_with_flags:
+			if name.lower().startswith(flag):
+				return "%s, %s" % (name[len(flag):], name[:len(flag)-1])
+		else:
+			pass
+		
+		return self.name
+
+
+
+class Address_v2(models.Model):
+	street = models.CharField(_("street"), max_length=128)
+	city = models.CharField(_("city"), max_length=64)
+	state = models.CharField(_("state"), max_length=2)
+	zip_code = models.CharField(_("zip code"), max_length=10)
+
+	def __unicode__ (self):
+		return self.street
+
+	def json(self):
+		return {
+			'street' :self.street,
+			'state' : self.state,
+			'city' : self.city,
+			'zip_code' : self.zip_code
+		}
+
+
+
+class Show_v2(models.Model):
+	title = models.CharField(max_length=100, blank=True)
+	headliners = models.CharField(_("headliners"), max_length=300, blank=True)
+	openers = models.CharField(_("openers"), max_length=400, blank=True)
+	website = models.CharField(max_length=200, blank=True)
+
+	date = models.DateTimeField()
+
+	venue = models.ForeignKey('Venue_v2')
+	
+	price = models.PositiveSmallIntegerField(default=0, blank=True)
+	ticket = models.URLField(blank=True)
+	soldout = models.BooleanField(default=False)
+	onsale = models.DateTimeField(blank=True)
+
+	age = models.PositiveSmallIntegerField(default=0, blank=True)
+
+
+	def __unicode__ (self):
+		return self.headliners
+
+	def json_min(self):
+		return {
+			'id' : self.id,
+			'title': self.title,
+			'headliners': self.headliners,
+			'date' : str(self.date),
+			'website' : self.website,
+			'venue' : self.venue.name,
+		}
+
+	def json_max(self):
+		return {
+			'id' : self.id,
+			'title': self.title,
+			'headliners': self.headliners,
+			'openers': self.openers,
+			'website' : self.website,
+			'date' : str(self.date),
+			'price' : self.price,
+			'ticket' : self.ticket,
+			'soldout' : self.soldout,
+			'onsale' : self.onsale,
+			'age' : self.age,
+			'venue' : self.venue.json()
+		}
+
+
+###################
+### OLD MODELS
 class Venue(models.Model):
 	name = models.CharField(max_length=200)
 	address = models.ForeignKey('Address')
