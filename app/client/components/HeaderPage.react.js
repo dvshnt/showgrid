021619b2c441183c69ad912ccc616909631e439d
Davis Hunt <vdh3@mac.com>
2015-04-20 15:24:51 -0500

    Header page update. Add new logo.

--- app/client/components/HeaderPage.react.js
+++ app/client/components/HeaderPage.react.js
@@ -4,10 +4,14 @@ var React = require('react');
 module.exports = HeaderPage = React.createClass({
 	render: function() {
 		return (
-			<section id="header--page">
-				<a href="/">SG</a>
-				<a href="/#/search"><div className="to--search"></div></a>
-			</section>
+			<div id="header__page">
+				<div id="header__page--logo"></div>
+				<span id="header__page--location">Nashville</span>
+				<div id="header__page--search">
+					<input className="search" type="text" placeholder="Search by venue or artist"/>
+					<input className="search" type="button"/>
+				</div>
+			</div>
 		)
 	}
 });
