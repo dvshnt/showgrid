55f007f1cfee0b458902ff86361c8b2246c28303
Davis Hunt <vdh3@mac.com>
2015-04-22 10:29:00 -0500

    Footer responds to scrolling now too.

--- app/client/components/Footer.react.js
+++ app/client/components/Footer.react.js
@@ -9,17 +9,17 @@ module.exports = Header = React.createClass({
 		};
 
 		return (
-			<footer>
+			<footer id="footer">
 				<div className="input--prompt">
 					<h3>something missing?</h3>
-					<p>Let us know if you notice a show or venue missing from the Grid. Otherwise, feel free to drop us a line or come visit us in our office located one hundred miles under the ground in an old, repurposed bomb shelter.</p>
+					<p>Let us know if you notice a show or venue missing from the Grid. Our goal is to make a rock solid event calendar without frills and we cannot do it alone. Otherwise, feel free to drop us a line or come visit us.</p>
 					<p>Email us at <a href="mailto:showgridnashville@gmail.com">showgridnashville@gmail.com</a>.</p>
 				</div>
 				<div className='email--form'>
 					<div id="mc_embed_signup">
 						<form action="//showgrid.us9.list-manage.com/subscribe/post?u=24f724359c2dd0e5e6d775b61&amp;id=49b59ce078" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
 							<div id="mc_embed_signup_scroll">
-								<label htmlFor="mce-EMAIL">Receive news and updates about Show Grid</label><br></br>
+								<label htmlFor="mce-EMAIL">Receive news and updates about Showgrid</label><br></br>
 								<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="Email"/>
 								<div style={ cookieStyle }>
 									<input type="text" name="b_24f724359c2dd0e5e6d775b61_49b59ce078" tabIndex="-1" value=""/>
