e55c80c74f52bc2dccb1bb4bbb5004733c24df77
Davis Hunt <vdh3@mac.com>
2015-05-01 12:25:26 -0500

    Center Venue information in table.

--- app/client/components/CalendarRowVenue.react.js
+++ app/client/components/CalendarRowVenue.react.js
@@ -7,15 +7,17 @@ module.exports = CalendarRowVenue = React.createClass({
 
 		return (
 			<div className="venue">
-				<div className={ this.props.venue.image_url }></div>
-	    	
-		    	<h3 className="name"><a href={ this.props.venue.website } target="_blank">{ this.props.venue.name }</a></h3>
+				<div className="venueInner">
+					<div className={ this.props.venue.image_url }></div>
 		    	
-		    	<div className="address">
-	    			{ this.props.venue.address.street }
-		    	</div>
-		    	<div className="address">
-		    		{ this.props.venue.address.city },  { this.props.venue.address.state } { this.props.venue.address.zip_code }
+			    	<h3 className="name"><a href={ this.props.venue.website } target="_blank">{ this.props.venue.name }</a></h3>
+			    	
+			    	<div className="address">
+		    			{ this.props.venue.address.street }
+			    	</div>
+			    	<div className="address">
+			    		{ this.props.venue.address.city },  { this.props.venue.address.state } { this.props.venue.address.zip_code }
+			    	</div>
 		    	</div>
 			</div>
 		)
