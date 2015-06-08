c1665111c3afb96a31f5695f12ac0e683a1a04d7
Davis Hunt <vdh3@mac.com>
2015-06-08 10:39:22 -0500

    onsale soon and recently added hooks.

--- app/server/urls.py
+++ app/server/urls.py
@@ -10,10 +10,15 @@ urlpatterns = patterns('',
     url(r'^$', 'server.views.index', name='index'),
     url(r'^check/venues$', 'server.views.check_venues', name='check_venues'),
 
+
     url(r'^i/search$', 'server.views.get_search_results', name='search results'),
     
  	url(r'^i/grid/$', 'server.views.grid', name='grid'),
-	url(r'^i/grid/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)$', 'server.views.grid', name='grid')
+	url(r'^i/grid/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)$', 'server.views.grid', name='grid'),
+
+ 	url(r'^i/onsale/$', 'server.views.shows_on_sale_soon', name='shows on sale soon'),
+
+ 	url(r'^i/recent/$', 'server.views.recently_added', name='recently added shows')
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
