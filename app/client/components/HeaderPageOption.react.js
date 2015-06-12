519260a4e4cf3ab7e4563caaf1855139b79891ea
Davis Hunt <vdh3@mac.com>
2015-06-12 10:08:06 -0500

    New header elements for search page options.

---
+++ app/client/components/HeaderPageOption.react.js
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+var $ = require('jquery'),
+	React = require('react');
+
+module.exports = HeaderPageOption = React.createClass({
+	render: function() {
+		var boxClass = (this.props.page === this.props.pageName) ? "header__page--option--box selected" : "header__page--option--box"; 
+
+		return (
+			<a className={ boxClass } href={ this.props.link } onClick={ this.props.selectPage.bind(this, this.props.pageName) }>
+				<b className={ this.props.symbol }></b>
+				<br></br>
+				<span>{ this.props.text }</span>
+			</a>
+		)
+	}
+});
