0684543bff49e6462ad1a891a13d17f3e72de39f
Davis Hunt <vdh3@mac.com>
2015-06-12 10:05:47 -0500

    New Header styles.

--- app/client/components/Header.react.js
+++ app/client/components/Header.react.js
@@ -2,15 +2,18 @@
 var React = require('react'),
 
 	HeaderPage = React.createFactory(require('./HeaderPage.react')),
-	HeaderCalendar = React.createFactory(require('./HeaderCalendar.react'));
+	HeaderCalendar = React.createFactory(require('./HeaderCalendar.react')),
+
+	HeaderSearch = React.createFactory(require('./HeaderSearch.react'));
 
 
 module.exports = Header = React.createClass({
 	render: function() {
 		return (
 			<header id="header">
-				<HeaderPage launchSearch={ this.props.launchSearch }/>
-				<HeaderCalendar days={ this.props.days } pickDate={ this.props.pickDate }/>
+				<HeaderPage selectPage={ this.props.selectPage } search={ this.props.search } page={ this.props.page }/>
+				<HeaderCalendar days={ this.props.days } pickDate={ this.props.pickDate } page={ this.props.page }/>
+				<HeaderSearch search={ this.props.search }  page={ this.props.page }/>
 			</header>
 		)
 	}
