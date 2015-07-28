554dc721f02b9041964b98694c776824db951579
Davis Hunt <vdh3@mac.com>
2015-07-28 10:44:07 -0500

    Created more robust mobile menu

---
+++ app/client/components/HeaderMobileMenu.react.js
@@ -0,0 +1,30 @@
+/** @jsx React.DOM */
+var $ = require('jquery'),
+	React = require('react');
+
+module.exports = HeaderMobileMenu = React.createClass({
+	render: function() {
+		return (
+			<div id="header__mobile--menu">
+				<header>
+					<aside className="profile">
+						<div></div>
+					</aside>
+					<div className="city"><span>Nashville</span></div>
+					<aside className="close" onClick={ this.props.closeMenu }><b className="icon-close"></b></aside>
+				</header>
+				<section>
+					<a href="/#/" className="page calendar" onClick={ this.props.selectPage.bind(this, 'calendar') }><b className="icon-calendar--full"></b></a>
+					<a href="/#/recent" className="page recent" onClick={ this.props.selectPage.bind(this, 'recent') }><b className="icon-recent--full"></b></a>
+					<a href="/#/featured" className="page star" onClick={ this.props.selectPage.bind(this, 'featured') }><b className="icon-star--full"></b></a>
+					<a href="/#/alerts" className="page alert" onClick={ this.props.selectPage.bind(this, 'alerts') }><b className="icon-bell--full"></b></a>
+					<a href="/#/favorites" className="page heart" onClick={ this.props.selectPage.bind(this, 'favorites') }><b className="icon-heart--full"></b></a>
+					<a href="/#/search" className="page search" onClick={ this.props.selectPage.bind(this, 'search') }><b className="icon-search"></b></a>
+				</section>
+				<footer>
+					
+				</footer>
+			</div>
+		)
+	}
+});
