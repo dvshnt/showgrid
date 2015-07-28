f25d860d077bfe6c4b51ce100973e7097a9324a0
Davis Hunt <vdh3@mac.com>
2015-07-28 10:48:29 -0500

    Featured tab.

--- app/client/components/Featured.react.js
+++ app/client/components/Featured.react.js
@@ -2,19 +2,50 @@
 var $ = require('jquery'),
 	React = require('react/addons'),
 
-	HeaderPage = React.createFactory(require('./HeaderPage.react')),
-
 	GridEngine = require('../util/GridEngine'),
+
+	FeaturedShow = React.createFactory(require('./FeaturedShow.react')),
 
 	moment = require('moment');
 
 module.exports = Featured = React.createClass({
+	getInitialState: function() {
+		return {
+			featured: []
+		}
+	},
+	
+	componentDidMount: function() {
+		this.getFeaturedShows();
+	},
+
+	getFeaturedShows: function() {
+		var _this = this; 
+
+		$.ajax({
+			type: "GET",
+			url: GridEngine.domain + "/i/featured"
+		}).success(function(data, status) {
+			_this.setState({
+				featured: data
+			});
+		});	
+	},
 
 	render: function() {
+		var results = [];
+
+		if (this.state.featured.length > 0) {
+			for (var i=0; i < this.state.featured.length; i++) {
+				var key = this.state.featured[i].id,
+					show = this.state.featured[i];
+
+				results.push(<FeaturedShow key={ key } show={ show }/>)
+			}
+		}
+
 		return (
-			<div className="recent--container">
-				<h1>Featured Shows</h1>
-			</div>
+			<div className="search--results rec">{ results }</div>
 		)
 	}
 });
