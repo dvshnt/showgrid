ea7c00654df2402048991a899dcb6f48cf58224f
Davis Hunt <vdh3@mac.com>
2015-07-28 10:47:56 -0500

    Added Recently added shows tab.

--- app/client/components/Recent.react.js
+++ app/client/components/Recent.react.js
@@ -2,19 +2,61 @@
 var $ = require('jquery'),
 	React = require('react/addons'),
 
-	HeaderPage = React.createFactory(require('./HeaderPage.react')),
-
 	GridEngine = require('../util/GridEngine'),
+
+	RecentDay = React.createFactory(require('./RecentDay.react')),
 
 	moment = require('moment');
 
 module.exports = Recent = React.createClass({
+	getInitialState: function() {
+		return {
+			page: 1,
+			recent: []
+		}
+	},
+	
+	componentDidMount: function() {
+		this.getRecentShows();
+	},
+
+	getRecentShows: function() {
+		var _this = this; 
+
+		$.ajax({
+			type: "GET",
+			url: GridEngine.domain + "/i/recent?page=" + this.state.page
+		}).success(function(data, status) {
+			if (status === "nocontent") {
+				$(".recent--results-more").remove();
+			}
+
+			_this.setState({
+				recent: _this.state.recent.concat(data)
+			});
+		});	
+
+		this.state.page += 1;
+	},
 
 	render: function() {
+		var results = [];
+
+		if (this.state.recent.length > 0) {
+			for (var i=0; i < this.state.recent.length; i++) {
+				var key = this.state.recent[i].id,
+					day = this.state.recent[i];
+
+				results.push(<RecentDay key={ key } day={ day }/>)
+			}
+		}
+
 		return (
-			<div className="recent--container">
-				<h1>Recently Added Shows</h1>
+			<div className="recent--results">
+				{ results }
+				<div className="recent--results-more" onClick={ this.getRecentShows }>See More New Shows...</div>
 			</div>
+
 		)
 	}
 });
