a37f6e06de82c401fd2a80c9adc1cc489cc0623f
Davis Hunt <vdh3@mac.com>
2015-07-28 10:49:04 -0500

    Preliminary Alerts and Favorites tabs.

---
+++ app/client/components/Alerts.React.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+var $ = require('jquery'),
+	React = require('react/addons');
+
+module.exports = Alerts = React.createClass({
+
+	render: function() {
+		return (
+			<div className="recent--container">
+				<h1>Alerts</h1>
+			</div>
+		)
+	}
+});
+
---
+++ app/client/components/Favorites.react.js
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var $ = require('jquery'),
+	React = require('react/addons');
+
+module.exports = Favorites = React.createClass({
+
+	render: function() {
+		return (
+			<div className="recent--container">
+				<h1>Favorite Shows</h1>
+			</div>
+		)
+	}
+});
