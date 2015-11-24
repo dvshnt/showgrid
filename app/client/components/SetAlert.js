36c6a700f90490b42e54338604843638068bd9ec
Davis Hunt <vdh3@mac.com>
2015-11-24 15:38:08 -0600

    Adding close button to alerts on mobile.

--- app/client/components/SetAlert.js
+++ app/client/components/SetAlert.js
@@ -176,6 +176,7 @@ class SetAlert extends Component {
 		return (
 			<b className={ className } onClick={ this.toggleAlert }>
 				<div className="alert-box">
+					<b id="close" className="icon-close" onClick={ this.props.toggleAlert }></b>
 					{ artistInfo }
 					Alert me
 					{ options }
