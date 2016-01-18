dbc7e5e81676e258c0dd4a13a13a3c5d6ce86f7e
arxii <yurisido@gmail.com>
2016-01-18 15:13:50 -0800

    add empty actions for pulling data for apis that will be added to the admin artists view

--- app/server/admin.py
+++ app/server/admin.py
@@ -7,9 +7,42 @@ class ShowAdmin(admin.ModelAdmin):
 	search_fields = ['headliners', 'openers', 'title']
 	list_display = ('date', 'headliners', 'openers', 'venue')
 
-admin.site.register(Address)
 
+
+
+#getter functions
+def pull_spotify(id,name):
+	pass
+
+def pull_echonest(id,name):
+	pass
+
+
+
+
+
+
+
+
+def pull_artist_data(modeladmin, request, queryset):
+    queryset.update(pulled=True)
+
+
+
+pull_artist_data.short_description = "Pull Artist Data"
+
+
+
+class ArtistAdmin(admin.ModelAdmin):
+	list_display = ['name', 'pulled','pulled_date','pulled_spotify','pulled_date']
+	ordering = ['name','pulled_date','pulled_spotify','pulled_echonest']
+	actions = [pull_artist_data]
+
+
+
+admin.site.register(Address)
 admin.site.register(Venue)
 admin.site.register(ShowgridUser)
 admin.site.register(Alert)
 admin.site.register(Show, ShowAdmin)
+admin.site.register(Artist, ArtistAdmin)
