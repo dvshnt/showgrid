86ba441cc4e5baa2aec92ffef84768dcc9f79d6c
arxii <yurisido@gmail.com>
2016-01-19 23:40:31 -0800

    add extract status actions for extracting names and for updating and pulling artist data

--- app/server/admin.py
+++ app/server/admin.py
@@ -5,15 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.conf.urls import patterns, include, url
 import random
-class ShowAdmin(admin.ModelAdmin):
-	search_fields = ['headliners', 'openers', 'title']
-	list_display = ('date', 'headliners', 'openers', 'venue')
-
-
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.template.response import TemplateResponse
-
 from threading import Thread
+from itertools import chain
+import requests
 
 
 
@@ -21,15 +17,50 @@ from threading import Thread
 
 
 
+
+def extract_artists_data(queryset):
+	shows = queryset.all()
+	for show in shows:
+		show.extract_artists_from_name()
+
+
+
+
+
+def update_artists_data(queryset):
+	artists = list(queryset)
+	for artist in artists:
+		artist.update_all()
 
 
 def get_artists_data(queryset):
-
 	artists = list(queryset)
-
 	for artist in artists:
-		artist.pull_spotify()
-		artist.pull_echonest()
+		artist.pull_all()
+
+
+
+def extract_artists_from_shows_action(modeladmin, request, queryset):
+	queryset.update(extract_queued=True)
+	selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)	
+	tr = Thread(target=extract_artists_data,args=(queryset,))
+	tr.start()
+	return HttpResponseRedirect('/admin/server/show/extractstatus/?ids=%s' % (",".join(selected)) );
+extract_artists_from_shows_action.short_description = "Extract artists"
+
+
+#only update thos
+def update_artist_data_action(modeladmin, request, queryset):
+	queryset.update(queued=True,pulled_spotify=False,pulled_echonest=False)
+	ct = ContentType.objects.get_for_model(queryset.model)
+	selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+	#que artist for update
+	#start thread
+	tr = Thread(target=update_artists_data,args=(queryset,))
+	tr.start()
+	return HttpResponseRedirect('/admin/server/artist/pullstatus/?ids=%s' % (",".join(selected)) );
+update_artist_data_action.short_description = "Update artist data"
+
 
 
 
@@ -42,7 +73,7 @@ def pull_artist_data_action(modeladmin, request, queryset):
 	selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
 	
 	#que artist for update
-	queryset.update(queued=True,pulled_spotify=False,pulled_echonest=False)
+	
 	
 	#start thread
 	tr = Thread(target=get_artists_data,args=(queryset,))
@@ -50,18 +81,63 @@ def pull_artist_data_action(modeladmin, request, queryset):
 
 	#redirect to update view status
 	return HttpResponseRedirect('/admin/server/artist/pullstatus/?ids=%s' % (",".join(selected)) );
+pull_artist_data_action.short_description = "Pull artist data"
 
 
+class ShowAdmin(admin.ModelAdmin):
+	search_fields = ['headliners', 'openers', 'title']
+	list_display = ('date', 'headliners', 'openers', 'venue')
+	actions = [extract_artists_from_shows_action]
+	def get_urls(self):
+		urls = super(ShowAdmin, self).get_urls()
+		my_urls = [
+			url(r'^extractstatus/$', self.extract_view),
+		]
+		return my_urls + urls
+	def extract_view(self, request):
+		ids = request.GET['ids'].split(',')
+		shows = []
+		done = 0
 
-pull_artist_data_action.short_description = "Pull Artist Data"
+		for i in ids:
+			show = Show.objects.get(id=i)
+			if show.extract_queued == False:
+				# print show.extract_queued
+				done += 1
+				done_artists = total_artists = show.headliner_artists.count() + show.opener_artists.count()
+			else:
+				headliners = show.headliner_artists.all()
+				openers = show.opener_artists.all()
+				total_artists = done_artists = 0
+				for a in openers:
+					total_artists += 1
+					if a.queued == False:
+						done_artists += 1
+				for a in headliners:
+					total_artists += 1
+					if a.queued == False:
+						done_artists += 1
 
+			shows.append({'name':show.headliners,'done_all':False if show.extract_queued else True,'artist_count':str(done_artists)+'/'+str(total_artists)})
 
+		context = dict(
+			self.admin_site.each_context(request),
+			done = True if len(shows) <= done else False,
+			count = str(done)+'/'+str(len(shows)),
+			shows = shows
+		)
+
+		return TemplateResponse(request, "admin/show_extract_status.html", context)
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ['title', 'external_url']
+	search_fields = ['title']
 
 class ArtistAdmin(admin.ModelAdmin):
-	list_display = ['name', 'pulled','pulled_date']
-	ordering = ['name','pulled_date','pulled']
-	fields = ('name', 'about')
-	actions = [pull_artist_data_action]
+	list_display = ['name','queued','pulled','pulled_date']
+	ordering = ['name','pulled_date','pulled','queued']
+	fields = ('name', 'bio','_images','_genres','facebook_url','twitter_url','articles')
+	actions = [pull_artist_data_action,update_artist_data_action]
 
 	def get_urls(self):
 		urls = super(ArtistAdmin, self).get_urls()
@@ -75,25 +151,14 @@ class ArtistAdmin(admin.ModelAdmin):
 
 	def status_view(self, request):
 		ids = request.GET['ids'].split(',')
-
 		artists = []
-		print ids
-
 		done = 0
 
-
 		for i in ids:
-		
-
 			artist = Artist.objects.get(id=i)
-
-			
 			if artist.queued == False:
 				done += 1
-
 			artists.append({'name':artist.name,'done_all':False if artist.queued else True,'done_spotify':artist.pulled_spotify,'done_echonest':artist.pulled_echonest})
-
-
 
 		context = dict(
 			self.admin_site.each_context(request),
@@ -111,5 +176,5 @@ admin.site.register(ShowgridUser)
 admin.site.register(Alert)
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Artist, ArtistAdmin)
-
+admin.site.register(Article, ArticleAdmin)
 
